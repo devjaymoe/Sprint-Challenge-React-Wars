9bb87ff3cf02d79d098a35ef6511a8ccdf7f094d
Devin_Moore <d.j.moore209@gmail.com>
2020-03-20 12:40:37 -0400

    styling for cards complete

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -1,28 +1,31 @@
 //https://swapi.co/api/people/
 import React from 'react';
 import styled from 'styled-components';
+import './StarWars.scss';
 
 const CardContainer = styled.div`
+    display:flex;
+    flex-direction: column;
     background-color: white;
     border-radius: 5px;
-    padding: 3% 5%;
-    margin: 3%;
+    padding: 1% 2%;
+    margin: 1%;
 `;
-
 
 const StarWars = ({character}) => {
     console.log(character)
     return (
         <CardContainer>
             <h3>{character.name}</h3>
-
             <div>
-                <p>{character.height}</p>
-                <p>{character.mass}</p>
-            </div>
-            <div>
-                <p>{character.birth_year}</p>
-                <p>{character.gender}</p>
+                <div className='section'>
+                    <p>Height: {character.height}</p>
+                    <p>Mass: {character.mass}</p>
+                </div>
+                <div className='section'>
+                    <p>Birth Year: {character.birth_year}</p>
+                    <p>Gender: {character.gender}</p>
+                </div>
             </div>
         </CardContainer>
     )
